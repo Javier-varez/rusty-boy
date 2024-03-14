a83440cb43e25be5de0a58642927e31619dc7fbf
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-03-14 07:18:29 +0100

    Complete decoding table

--- opcode_game.py
+++ opcode_game.py
@@ -4,15 +4,8 @@ import json
 from enum import IntEnum
 from functools import reduce
 from argparse import ArgumentParser
-
-class Register(IntEnum):
-    A = 7
-    B = 0
-    C = 1
-    D = 2
-    E = 3
-    H = 4
-    L = 5
+from prettytable import PrettyTable
+from colorama import Fore
 
 class Opcode:
     def __init__(self, mnemonic, operands):
@@ -87,6 +80,49 @@ class Pattern:
     def __repr__(self):
         return f'Pattern({self.pattern})'
 
+class Register(IntEnum):
+    A = 7
+    B = 0
+    C = 1
+    D = 2
+    E = 3
+    H = 4
+    L = 5
+
+class RegisterPair(IntEnum):
+    BC = 0
+    DE = 1
+    HL = 2
+    SP = 3
+
+class RegisterPairStack(IntEnum):
+    BC = 0
+    DE = 1
+    HL = 2
+    AF = 3
+
+class RegisterPairMem(IntEnum):
+    BC = 0
+    DE = 1
+    HLINC = 2
+    HLDEC = 3
+
+class Condition(IntEnum):
+    NZ = 0
+    Z = 1
+    NC = 2
+    C = 3
+
+class ResetTarget(IntEnum):
+    Target0x00 = 0
+    Target0x08 = 1
+    Target0x10 = 2
+    Target0x18 = 3
+    Target0x20 = 4
+    Target0x28 = 5
+    Target0x30 = 6
+    Target0x38 = 7
+
 patterns = [
     (Pattern('01rrrRRR'), Binding('r', Register), Binding('R', Register)),
     (Pattern('01rrr110'), Binding('r', Register)),
@@ -94,6 +130,87 @@ patterns = [
     (Pattern('01110110'),),
     (Pattern('00rrr110'), Binding('r', Register)),
     (Pattern('00110110'),),
+    (Pattern('10000rrr'), Binding('r', Register)),
+    (Pattern('10010rrr'), Binding('r', Register)),
+    (Pattern('10100rrr'), Binding('r', Register)),
+    (Pattern('10110rrr'), Binding('r', Register)),
+    (Pattern('10001rrr'), Binding('r', Register)),
+    (Pattern('10011rrr'), Binding('r', Register)),
+    (Pattern('10101rrr'), Binding('r', Register)),
+    (Pattern('10111rrr'), Binding('r', Register)),
+    (Pattern('00RRR100'), Binding('R', Register)),
+    (Pattern('00RRR101'), Binding('R', Register)),
+    (Pattern('11001011'),),
+    (Pattern('00000000'),),
+    (Pattern('00RR0001'), Binding('R', RegisterPair)),
+    (Pattern('00RR0010'), Binding('R', RegisterPairMem)),
+    (Pattern('00RR1010'), Binding('R', RegisterPairMem)),
+    (Pattern('00001000'),),
+    (Pattern('00RR0011'), Binding('R', RegisterPair)),
+    (Pattern('00RR1011'), Binding('R', RegisterPair)),
+    (Pattern('00rr1001'), Binding('r', RegisterPair)),
+    (Pattern('00000111'),),
+    (Pattern('00001111'),),
+    (Pattern('00010111'),),
+    (Pattern('00011111'),),
+    (Pattern('00100111'),),
+    (Pattern('00101111'),),
+    (Pattern('00110111'),),
+    (Pattern('00111111'),),
+    (Pattern('00011000'),),
+    (Pattern('001cc000'), Binding('c', Condition)),
+    (Pattern('00010000'),),
+    (Pattern('11000110'),),
+    (Pattern('11001110'),),
+    (Pattern('11010110'),),
+    (Pattern('11011110'),),
+    (Pattern('11100110'),),
+    (Pattern('11101110'),),
+    (Pattern('11110110'),),
+    (Pattern('11111110'),),
+    (Pattern('110cc000'), Binding('c', Condition)),
+    (Pattern('11001001'),),
+    (Pattern('11011001'),),
+    (Pattern('110cc010'), Binding('c', Condition)),
+    (Pattern('11000011'),),
+    (Pattern('11101001'),),
+    (Pattern('110cc100'), Binding('c', Condition)),
+    (Pattern('11001101'),),
+    (Pattern('11ttt111'), Binding('t', ResetTarget)),
+    (Pattern('11rr0001'), Binding('r', RegisterPairStack)),
+    (Pattern('11rr0101'), Binding('r', RegisterPairStack)),
+    (Pattern('11100010'),),
+    (Pattern('11110010'),),
+    (Pattern('11100000'),),
+    (Pattern('11110000'),),
+    (Pattern('11101010'),),
+    (Pattern('11111010'),),
+    (Pattern('11101000'),),
+    (Pattern('11111000'),),
+    (Pattern('11111001'),),
+    (Pattern('11110011'),),
+    (Pattern('11111011'),),
+    (Pattern('00110100'),),
+    (Pattern('00110101'),),
+    (Pattern('10000110'),),
+    (Pattern('10001110'),),
+    (Pattern('10010110'),),
+    (Pattern('10011110'),),
+    (Pattern('10100110'),),
+    (Pattern('10101110'),),
+    (Pattern('10110110'),),
+    (Pattern('10111110'),),
+    (Pattern('11010011'),),
+    (Pattern('11100011'),),
+    (Pattern('11100100'),),
+    (Pattern('11110100'),),
+    (Pattern('11011011'),),
+    (Pattern('11101011'),),
+    (Pattern('11101100'),),
+    (Pattern('11111100'),),
+    (Pattern('11011101'),),
+    (Pattern('11101101'),),
+    (Pattern('11111101'),),
 ]
 
 def ls_opcodes(args):
@@ -114,6 +231,35 @@ def ls_opcodes(args):
         else:
             print(f"{i:#x} = {opcode}, implemented = {opcode.is_implemented()}")
 
+def opcode_table(args):
+    opcodes = read_opcodes()
+
+    resolved = []
+    for pattern, *bindings in patterns:
+        resolved.extend(pattern.resolve_all(bindings))
+
+    # mark as implemented
+    for p in resolved:
+        if opcodes[p.as_opcode()].is_implemented():
+            raise Exception(f'conflicting generation of opcode: {p.as_opcode()}')
+        opcodes[p.as_opcode()].implemented = True
+
+    table = PrettyTable()
+    table.header = False
+
+    table.add_row([f"x{i-1:x}" if i > 0 else ' ' for i in range(17)])
+    for i in range(16):
+        row = [f"{i:x}x"]
+        for j in range(16):
+            op = opcodes[i * 16 + j]
+            if op.is_implemented():
+                row.append(f"{Fore.GREEN}{op}{Fore.WHITE}")
+            else:
+                row.append(f"{Fore.RED}{op}{Fore.WHITE}")
+        table.add_row(row)
+    print(table)
+
+
 def default_handler(args):
     print('No command selected')
 
@@ -125,6 +271,9 @@ if __name__ == '__main__':
     ls_parser = subparsers.add_parser('ls')
     ls_parser.add_argument('-u', action='store_true')
     ls_parser.set_defaults(handler=ls_opcodes)
+
+    table_parser = subparsers.add_parser('table')
+    table_parser.set_defaults(handler=opcode_table)
 
     args = parser.parse_args()
     args.handler(args)
--- sm83/src/decoder.rs
+++ sm83/src/decoder.rs
@@ -1,12 +1,83 @@
 use sm83_decoder_macros::generate_decoder_table;
 
 pub enum OpCode {
-    Ld8RegReg(Register, Register),     // ld Register, Register
-    Ld8RegImm(Register),               // ld Register, n8
-    Ld8RegInd(Register, RegisterPair), // ld Register, [RegisterPair]
-    Ld8IndReg(RegisterPair, Register), // ld [RegisterPair], Register
-    Ld8IndImm(RegisterPair),           // ld [RegisterPair], n8
-    Halt,                              // halt
+    Ld8RegReg(Register, Register),                 // ld Register, Register
+    Ld8RegImm(Register),                           // ld Register, n8
+    Ld8RegInd(Register, RegisterPair),             // ld Register, [RegisterPair]
+    Ld8IndReg(RegisterPair, Register),             // ld [RegisterPair], Register
+    Ld8IndImm(RegisterPair),                       // ld [RegisterPair], n8
+    Ld8IndAcc(RegisterPairMem),                    // ld [RegisterPairMem], A
+    Ld8AccInd(RegisterPairMem),                    // ld A, [RegisterPairMem]
+    Ld8ZeroPageCAcc,                               // ld [C], A
+    Ld8AccZeroPageC,                               // ld A, [C]
+    Ld8ZeroPageImmAcc,                             // ld [n8], A
+    Ld8AccZeroPageImm,                             // ld A, [n8]
+    Ld8IndImmAcc,                                  // ld [n16], A
+    Ld8AccIndImm,                                  // ld A, [n16]
+    Ld16RegImm(RegisterPair),                      // ld RegisterPair, n16
+    Ld16IndImmSp,                                  // ld [a16], SP
+    Ld16HlSpImm,                                   // ld HL, SP + n8
+    Ld16SpHl,                                      // ld SP, HL
+    Halt,                                          // halt
+    AddRegReg(Register, Register),                 // add Register, Register
+    SubRegReg(Register, Register),                 // sub Register, Register
+    AndRegReg(Register, Register),                 // and Register, Register
+    OrRegReg(Register, Register),                  // or Register, Register
+    AdcRegReg(Register, Register),                 // adc Register, Register
+    SbcRegReg(Register, Register),                 // sbc Register, Register
+    XorRegReg(Register, Register),                 // xor Register, Register
+    CpRegReg(Register, Register),                  // cp Register, Register
+    AddRegPairRegPair(RegisterPair, RegisterPair), // add Register, Register
+    AddAccImm,                                     // add A, n8
+    AdcAccImm,                                     // adc A, n8
+    SubAccImm,                                     // sub A, n8
+    SbcAccImm,                                     // sbc A, n8
+    AndAccImm,                                     // and A, n8
+    XorAccImm,                                     // xor A, n8
+    OrAccImm,                                      // or A, n8
+    CpAccImm,                                      // cp A, n8
+    AddSpImm,                                      // add SP, n8
+    AddAccHlInd,                                   // add A, [HL]
+    AdcAccHlInd,                                   // adc A, [HL]
+    SubAccHlInd,                                   // sub A, [HL]
+    SbcAccHlInd,                                   // sbc A, [HL]
+    AndAccHlInd,                                   // and A, [HL]
+    XorAccHlInd,                                   // xor A, [HL]
+    OrAccHlInd,                                    // or A, [HL]
+    CpAccHlInd,                                    // cp A, [HL]
+    IncReg(Register),                              // inc Register
+    DecReg(Register),                              // dec Register
+    IncRegPair(RegisterPair),                      // inc RegisterPair
+    DecRegPair(RegisterPair),                      // inc RegisterPair
+    IncIndHl,                                      // inc [HL]
+    DecIndHl,                                      // dec [HL]
+    Prefix,                                        // prefix
+    Nop,                                           // nop
+    Rlca,                                          // rlca
+    Rrca,                                          // rrca
+    Rla,                                           // rla
+    Rra,                                           // rra
+    Daa,                                           // daa
+    Cpl,                                           // cpl
+    Scf,                                           // scf
+    Ccf,                                           // ccf
+    JrImm,                                         // jr imm8
+    JrCondImm(Condition),                          // jr cond, imm8
+    Stop,                                          // stop
+    RetCond(Condition),                            // ret cond
+    Ret,                                           // ret
+    Reti,                                          // reti
+    JpCondImm(Condition),                          // jp cond, imm16
+    JpImm,                                         // jp imm16
+    JpHl,                                          // jp HL
+    CallCondImm(Condition),                        // call cond, imm16
+    CallImm,                                       // call imm16
+    Reset(ResetTarget),                            // reset target
+    Pop(RegisterPairStack),                        // pop RegisterPairStack
+    Push(RegisterPairStack),                       // push RegisterPairStack
+    Di,                                            // di
+    Ei,                                            // ei
+    Illegal,                                       // Illegal
 }
 
 // r => destination reg
@@ -24,12 +95,38 @@ generate_decoder_table! {
             L = 5,
         },
         RegisterPair {
-            AF = 0,
-            BC = 1,
-            DE = 2,
-            HL = 3,
-            SP = 4,
-            PC = 5,
+            BC = 0,
+            DE = 1,
+            HL = 2,
+            SP = 3,
+        },
+        RegisterPairStack {
+            BC = 0,
+            DE = 1,
+            HL = 2,
+            AF = 3,
+        },
+        RegisterPairMem {
+            BC = 0,
+            DE = 1,
+            HLINC = 2,
+            HLDEC = 3,
+        },
+        Condition {
+            NZ = 0,
+            Z = 1,
+            NC = 2,
+            C = 3,
+        },
+        ResetTarget {
+            Addr0x00 = 0,
+            Addr0x08 = 1,
+            Addr0x10 = 2,
+            Addr0x18 = 3,
+            Addr0x20 = 4,
+            Addr0x28 = 5,
+            Addr0x30 = 6,
+            Addr0x38 = 7,
         }
     }
     DECODER_TABLE: [OpCode; 256] {
@@ -39,5 +136,88 @@ generate_decoder_table! {
         [] "01110110" => { OpCode::Halt },
         [r: Register] "00rrr110" => { OpCode::Ld8RegImm(#r) },
         [] "00110110" => { OpCode::Ld8IndImm(RegisterPair::HL) },
+        [r: Register] "10000rrr" => { OpCode::AddRegReg(Register::A, #r) },
+        [r: Register] "10010rrr" => { OpCode::SubRegReg(Register::A, #r) },
+        [r: Register] "10100rrr" => { OpCode::AndRegReg(Register::A, #r) },
+        [r: Register] "10110rrr" => { OpCode::OrRegReg(Register::A, #r) },
+        [r: Register] "10001rrr" => { OpCode::AdcRegReg(Register::A, #r) },
+        [r: Register] "10011rrr" => { OpCode::SbcRegReg(Register::A, #r) },
+        [r: Register] "10101rrr" => { OpCode::XorRegReg(Register::A, #r) },
+        [r: Register] "10111rrr" => { OpCode::CpRegReg(Register::A, #r) },
+        [R: Register] "00RRR100" => { OpCode::IncReg(#R) },
+        [R: Register] "00RRR101" => { OpCode::DecReg(#R) },
+        [] "11001011" => { OpCode::Prefix },
+        [] "00000000" => { OpCode::Nop },
+        [R: RegisterPair] "00RR0001" => { OpCode::Ld16RegImm(#R) },
+        [R: RegisterPairMem] "00RR0010" => { OpCode::Ld8IndAcc(#R) },
+        [R: RegisterPairMem] "00RR1010" => { OpCode::Ld8AccInd(#R) },
+        [] "00001000" => { OpCode::Ld16IndImmSp },
+        [R: RegisterPair] "00RR0011" => { OpCode::IncRegPair(#R) },
+        [R: RegisterPair] "00RR1011" => { OpCode::DecRegPair(#R) },
+        [R: RegisterPair] "00RR1001" => { OpCode::AddRegPairRegPair(RegisterPair::HL, #R) },
+        [] "00000111" => { OpCode::Rlca },
+        [] "00001111" => { OpCode::Rrca },
+        [] "00010111" => { OpCode::Rla },
+        [] "00011111" => { OpCode::Rra },
+        [] "00100111" => { OpCode::Daa },
+        [] "00101111" => { OpCode::Cpl },
+        [] "00110111" => { OpCode::Scf },
+        [] "00111111" => { OpCode::Ccf },
+        [] "00011000" => { OpCode::JrImm },
+        [c: Condition] "001cc000" => { OpCode::JrCondImm(#c) },
+        [] "00010000" => { OpCode::Stop },
+        [] "11000110" => { OpCode::AddAccImm },
+        [] "11001110" => { OpCode::AdcAccImm },
+        [] "11010110" => { OpCode::SubAccImm },
+        [] "11011110" => { OpCode::SbcAccImm },
+        [] "11100110" => { OpCode::AndAccImm },
+        [] "11101110" => { OpCode::XorAccImm },
+        [] "11110110" => { OpCode::OrAccImm },
+        [] "11111110" => { OpCode::CpAccImm },
+        [c: Condition] "110cc000" => { OpCode::RetCond(#c) },
+        [] "11001001" => { OpCode::Ret },
+        [] "11011001" => { OpCode::Reti },
+        [c: Condition] "110cc010" => { OpCode::JpCondImm(#c) },
+        [] "11000011" => { OpCode::JpImm },
+        [] "11101001" => { OpCode::JpHl },
+        [c: Condition] "110cc100" => { OpCode::CallCondImm(#c) },
+        [] "11001101" => { OpCode::CallImm },
+        [t: ResetTarget] "11ttt111" => { OpCode::Reset(#t) },
+        [r: RegisterPairStack] "11rr0001" => { OpCode::Pop(#r) },
+        [r: RegisterPairStack] "11rr0101" => { OpCode::Push(#r) },
+        [] "11100010" => { OpCode::Ld8ZeroPageCAcc },
+        [] "11110010" => { OpCode::Ld8AccZeroPageC },
+        [] "11100000" => { OpCode::Ld8ZeroPageImmAcc },
+        [] "11110000" => { OpCode::Ld8AccZeroPageImm },
+        [] "11101010" => { OpCode::Ld8IndImmAcc },
+        [] "11111010" => { OpCode::Ld8AccIndImm },
+        [] "11101000" => { OpCode::AddSpImm },
+        [] "11111000" => { OpCode::Ld16HlSpImm },
+        [] "11111001" => { OpCode::Ld16SpHl },
+        [] "11110011" => { OpCode::Di },
+        [] "11111011" => { OpCode::Ei },
+        [] "00110100" => { OpCode::IncIndHl },
+        [] "00110101" => { OpCode::DecIndHl },
+        [] "10000110" => { OpCode::AddAccHlInd },
+        [] "10001110" => { OpCode::AdcAccHlInd },
+        [] "10010110" => { OpCode::SubAccHlInd },
+        [] "10011110" => { OpCode::SbcAccHlInd },
+        [] "10100110" => { OpCode::AndAccHlInd },
+        [] "10101110" => { OpCode::XorAccHlInd },
+        [] "10110110" => { OpCode::OrAccHlInd },
+        [] "10111110" => { OpCode::CpAccHlInd },
+
+        [] "11010011" => { OpCode::Illegal },
+        [] "11100011" => { OpCode::Illegal },
+        [] "11100100" => { OpCode::Illegal },
+        [] "11110100" => { OpCode::Illegal },
+
+        [] "11011011" => { OpCode::Illegal },
+        [] "11101011" => { OpCode::Illegal },
+        [] "11101100" => { OpCode::Illegal },
+        [] "11111100" => { OpCode::Illegal },
+        [] "11011101" => { OpCode::Illegal },
+        [] "11101101" => { OpCode::Illegal },
+        [] "11111101" => { OpCode::Illegal },
     }
 }
