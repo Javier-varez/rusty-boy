99ae7d3b99446976fd2423c8f8fd6414c092b213
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-04-11 21:52:02 +0200

    CLI args, cpu usage and fps control

--- rusty-boy/src/main.rs
+++ rusty-boy/src/main.rs
@@ -5,12 +5,26 @@ mod rusty_boy;
 use std::io::BufWriter;
 use std::path::PathBuf;
 use std::str::FromStr;
+use std::time::{Duration, Instant};
 
+use clap::Parser;
 use ppu::{Color, DISPLAY_HEIGHT, DISPLAY_WIDTH};
 
 use rusty_boy::RustyBoy;
 
-fn _save_png(idx: usize, frame: &[[Color; DISPLAY_WIDTH]; DISPLAY_HEIGHT]) -> anyhow::Result<()> {
+/// Runs the given Game Boy emulator ROM
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// The ROM to run
+    rom_path: PathBuf,
+
+    /// Saves PNG files with each frame to the current directory
+    #[arg(short)]
+    save_pngs: bool,
+}
+
+fn save_png(idx: usize, frame: &[[Color; DISPLAY_WIDTH]; DISPLAY_HEIGHT]) -> anyhow::Result<()> {
     let path = PathBuf::from_str(&format!("frame_{idx}.png"))?;
 
     const MAX: u8 = 255;
@@ -39,9 +53,17 @@ fn _save_png(idx: usize, frame: &[[Color; DISPLAY_WIDTH]; DISPLAY_HEIGHT]) -> an
     Ok(())
 }
 
+pub fn sleep_until(deadline: Instant) {
+    let now = Instant::now();
+    if let Some(delay) = deadline.checked_duration_since(now) {
+        std::thread::sleep(delay);
+    }
+}
+
 fn main() -> anyhow::Result<()> {
-    let path = PathBuf::from_str("main.gb")?;
-    let mut rusty_boy = RustyBoy::new_with_rom(&path)?;
+    let args = Args::parse();
+
+    let mut rusty_boy = RustyBoy::new_with_rom(&args.rom_path)?;
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsys = sdl_context.video().unwrap();
@@ -56,7 +78,12 @@ fn main() -> anyhow::Result<()> {
     canvas.clear();
     canvas.present();
 
+    let mut frame_id = 0;
     let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let mut next_deadline = Instant::now();
+    let mut start = Instant::now();
+    let mut load = Duration::from_millis(0);
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -69,7 +96,18 @@ fn main() -> anyhow::Result<()> {
             }
         }
 
-        let frame = rusty_boy.run_until_next_frame().unwrap();
+        let frame = {
+            let frame_start = Instant::now();
+            let frame = rusty_boy.run_until_next_frame().unwrap();
+            let frame_end = Instant::now();
+            load += frame_end - frame_start;
+            frame
+        };
+
+        if args.save_pngs {
+            save_png(frame_id, frame)?;
+        }
+
         for (y, line) in frame.iter().enumerate() {
             for (x, p) in line.iter().enumerate() {
                 const MAX: u8 = 255;
@@ -87,7 +125,22 @@ fn main() -> anyhow::Result<()> {
         }
 
         canvas.present();
-        std::thread::sleep(std::time::Duration::new(0, 1_000_000_000u32 / 60));
+
+        {
+            let now = Instant::now();
+            let duration = now - start;
+            if duration > Duration::from_secs(1) {
+                let load_pct = load.as_nanos() as f64 / duration.as_nanos() as f64 * 100.0;
+                println!("CPU usage is {}", load_pct);
+                start = now;
+                load = Duration::from_secs(0);
+            }
+        }
+
+        next_deadline += Duration::from_nanos(16_666_667); // 60 fps
+        sleep_until(next_deadline);
+
+        frame_id += 1;
     }
 
     Ok(())
