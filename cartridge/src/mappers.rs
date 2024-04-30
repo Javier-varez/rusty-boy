098835e74a58dbdec72e34b102c6d19d64afe987
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-05-01 01:01:10 +0200

    MB3 support

--- cartridge/src/mappers.rs
+++ cartridge/src/mappers.rs
@@ -1,11 +1,13 @@
 use crate::header::{CartridgeHeader, CartridgeType};
 
 mod mbc1;
+mod mbc3;
 mod rom_only;
 
 pub enum Mapper<'a> {
     RomOnly(rom_only::RomOnly<'a>),
     Mbc1(mbc1::Mbc1<'a>),
+    Mbc3(mbc3::Mbc3<'a>),
 }
 
 impl<'a> Mapper<'a> {
@@ -14,6 +16,9 @@ impl<'a> Mapper<'a> {
             CartridgeType::RomOnly => Self::RomOnly(rom_only::RomOnly::new(data)),
             CartridgeType::Mbc1 | CartridgeType::Mbc1Ram | CartridgeType::Mbc1RamBattery => {
                 Self::Mbc1(mbc1::Mbc1::new(data, header.ram_size.into_usize().unwrap()))
+            }
+            CartridgeType::Mbc3 | CartridgeType::Mbc3Ram | CartridgeType::Mbc3RamBattery => {
+                Self::Mbc3(mbc3::Mbc3::new(data, header.ram_size.into_usize().unwrap()))
             }
             v => {
                 // Other cartridge types are currently unsupported
@@ -26,6 +31,7 @@ impl<'a> Mapper<'a> {
         match self {
             Self::RomOnly(rom_only) => rom_only.read(address),
             Self::Mbc1(mbc1) => mbc1.read(address),
+            Self::Mbc3(mbc3) => mbc3.read(address),
         }
     }
 
@@ -33,6 +39,7 @@ impl<'a> Mapper<'a> {
         match self {
             Self::RomOnly(rom_only) => rom_only.write(address, value),
             Self::Mbc1(mbc1) => mbc1.write(address, value),
+            Self::Mbc3(mbc3) => mbc3.write(address, value),
         }
     }
 }
